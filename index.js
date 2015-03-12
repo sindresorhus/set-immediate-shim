2e4f373a35ccbf2afd62250800e6f337eca8a9cb
andi-neck <andi.neck@intesso.com>
2015-03-12 07:21:12 +0100

    indent

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = typeof setImmediate === 'function' ? setImmediate :
-		  function setImmediate(fn) {
-		    var args = [].slice.apply(arguments).splice(1, 0, 0);
-		    setTimeout.apply(null, args);
-		  };
+	function setImmediate(fn) {
+		var args = [].slice.apply(arguments).splice(1, 0, 0);
+		setTimeout.apply(null, args);
+	};
