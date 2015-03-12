325d783df5cf1c1b683a152e6017b94b41adeb3c
andi-neck <andi.neck@intesso.com>
2015-03-12 07:02:40 +0100

    Revert 773b303..9b0adc4
    This rolls back to commit 773b303ec669cffca63124e4bd4b426b06bcccde.

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 'use strict';
 module.exports = typeof setImmediate === 'function' ? setImmediate :
-  function setImmediate() {
-    var args = Array.prototype.slice.apply(arguments);
-    var fn = args.shift();
-    console.log('fn', fn, 'args', args);
-    setTimeout(function() {
-      fn.apply(null, args);
-    }, 0);
-  };
+	function setImmediate(fn) { setTimeout(fn, 0); };
--- test.js
+++ test.js
@@ -18,21 +18,3 @@ test(function (t) {
 
 	t.assert(!called);
 });
-
-test(function (t) {
-	var called = false;
-
-	// force the shim
-	var _ = setImmediate;
-	setImmediate = null;
-	var setImmediateShim = requireUncached('./');
-	setImmediate = _;
-
-	setImmediateShim(function (a, b) {
-		var max = Math.max(a, b);
-		console.log('max', max)
-		t.assert(max === 5);
-		t.end();
-	}, 3, 5);
-	t.assert(!called);
-});
