56c84108244ead2ccf6b15ad2b56b5b7f080f657
andi-neck <andi.neck@intesso.com>
2015-03-12 07:14:25 +0100

    applies setImmediate arguments to setImmediate

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 'use strict';
 module.exports = typeof setImmediate === 'function' ? setImmediate :
-	function setImmediate(fn) { setTimeout(fn, 0); };
+  function setImmediate(fn) {
+    var args = [].slice.apply(arguments).splice(1, 0, 0);
+    setTimeout.apply(null, args);
+  };
