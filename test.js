cf2a35e5cf633b1a6abedb0d6f78d8303d6fdd6f
andi-neck <andi.neck@intesso.com>
2015-03-12 07:19:17 +0100

    fix formatting

--- test.js
+++ test.js
@@ -3,36 +3,37 @@ var test = require('ava');
 var requireUncached = require('require-uncached');
 
 test(function (t) {
-  var called = false;
+	var called = false;
 
-  // force the shim
-  var _ = setImmediate;
-  setImmediate = null;
-  var setImmediateShim = requireUncached('./');
-  setImmediate = _;
+	// force the shim
+	var _ = setImmediate;
+	setImmediate = null;
+	var setImmediateShim = requireUncached('./');
+	setImmediate = _;
 
-  setImmediateShim(function () {
-    called = true;
-    t.end();
-  });
+	setImmediateShim(function () {
+		called = true;
+		t.end();
+	});
 
-  t.assert(!called);
+	t.assert(!called);
 });
 
 test(function (t) {
-  var called = false;
+	var called = false;
 
-  // force the shim
-  var _ = setImmediate;
-  setImmediate = null;
-  var setImmediateShim = requireUncached('./');
-  setImmediate = _;
+	// force the shim
+	var _ = setImmediate;
+	setImmediate = null;
+	var setImmediateShim = requireUncached('./');
+	setImmediate = _;
 
-  setImmediateShim(function (a, b) {
-    var max = Math.max(a, b);
-    console.log('max', max)
-    t.assert(max === 5);
-    t.end();
-  }, 3, 5);
-  t.assert(!called);
+	setImmediateShim(function (a, b) {
+		var max = Math.max(a, b);
+		console.log('max', max)
+		t.assert(max === 5);
+		t.end();
+	}, 3, 5);
+
+	t.assert(!called);
 });
